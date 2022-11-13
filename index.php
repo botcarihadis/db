8a68e6247c8351f5a58b24f5dcf3626431d53576
dananghp7 <115846679+botcarihadis@users.noreply.github.com>
2022-11-13 14:58:08 +0000

    1-8 kitab

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
-<html>
-  <head>
-    <title>PHP Test</title>
-  </head>
-  <body>
-    <?php echo '<p>Hello World</p>'; ?> 
-
-    <!--
-    This script places a badge on your repl's full-browser view back to your repl's cover
-    page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
-    teal, blue, blurple, magenta, pink!
-    -->
-    <script src="https://replit.com/public/js/replit-badge.js" theme="blue" defer></script> 
-  </body>
-</html>
+<?php
+$file = 'hadisdb/Shahih_Bukhari';
+$array = file($file);
+foreach($array as $no => $baris){
+  if(strpos($baris, 'puasa') !== false){
+    echo $no." ";
+    // break;
+  }
+}
